bf6cf9953a10a16653616d5052acba581093d560
Radosław Wieczorek <radoslaw.wieczorek24@gmail.com>
2015-05-16 17:50:06 +0200

    Add user following

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     # Make an ivalid micropost
     assert_no_difference "Micropost.count" do
       post microposts_path, micropost: { content: "" }
@@ -16,20 +17,36 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select "div#error_explanation"
     # Valid submission
     content = "This is a valid micropost"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference "Micropost.count", 1 do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert first_micropost.picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
     # Delete a post
     assert_select 'a', text: 'delete'
-    first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
       delete micropost_path(first_micropost)
     end
     # Visit a dfferent user
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    # User with 0 microposts
+    other_user = users(:mallory)
+    log_in_as(other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    other_user.microposts.create(content: "A micropost")
+    get root_path
+    assert_match "1 micropost", response.body
   end
 end
